c38e90d2f8d2490e4b91d152b6ee9f1dc570bb17
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2025-01-13 13:29:24 +0200

    ÜL 5 / currency format

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,12 +1,16 @@
 const MealItem = (props) => {
-    console.log(props)
+    const format = (num) => {
+        return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
+            num,
+        )
+    }
     return (
         <li>
             <article class="meal-item">
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name} />
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p class="meal-item-price">{props.meal.price}</p>
+                    <p class="meal-item-price">{format(props.meal.price)}</p>
                     <p class="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p class="meal-item-actions">
